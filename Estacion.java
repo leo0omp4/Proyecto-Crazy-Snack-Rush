a0fef045f61c3e32b582484ffb0c7c228b7687f9
leo0omp4 <agsolu16@gmail.com>
2026-06-23 15:47:13 -0600

    Add files via upload
    clase Estacion base con listas de aceptacion

---
+++ Estacion.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Estacion {
+    private String nombre;
+    private List<Ingrediente> ingredientesAceptados;
+    private List<Receta> recetasDisponibles;
+
+    // Constructor
+    public Estacion(String nombre) {
+        this.nombre = nombre;
+        this.ingredientesAceptados = new ArrayList<>();
+        this.recetasDisponibles = new ArrayList<>();
+    }
+
+    // Método que pide el UML
+    public Receta generarReceta() {
+        // Por ahora retorna null, luego pondremos la lógica para la estación de entrega
+        return null; 
+    }
+
+    // Getters y Setters
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public List<Ingrediente> getIngredientesAceptados() { return ingredientesAceptados; }
+    public void setIngredientesAceptados(List<Ingrediente> ingredientesAceptados) { this.ingredientesAceptados = ingredientesAceptados; }
+
+    public List<Receta> getRecetasDisponibles() { return recetasDisponibles; }
+    public void setRecetasDisponibles(List<Receta> recetasDisponibles) { this.recetasDisponibles = recetasDisponibles; }
+}
