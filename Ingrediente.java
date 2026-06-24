d66d4e89254d4a7e98d47cd18b76fa1bb0af20c2
leo0omp4 <agsolu16@gmail.com>
2026-06-24 08:48:11 -0600

    Add files via upload

--- Ingrediente.java
+++ Ingrediente.java
@@ -1,15 +1,28 @@
 public class Ingrediente {
     private String nombre;
     private String estado;
+    private int tiempoEnProceso = 0;
 
     public Ingrediente(String nombre) {
         this.nombre = nombre;
         this.estado = "crudo";
+        this.tiempoEnProceso = 0;
     }
 
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-    
+    // Getters y Setters
     public String getEstado() { return estado; }
     public void setEstado(String estado) { this.estado = estado; }
+    
+    public int getTiempoEnProceso() { return tiempoEnProceso; }
+    public void setTiempoEnProceso(int tiempo) { this.tiempoEnProceso = tiempo; }
+    
+    public void incrementarTiempo() { this.tiempoEnProceso++; }
+
+    public String getNombre() {
+    return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
